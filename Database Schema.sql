5d9e1e434fdfa4ad23ecab311522979c27baf2e9
VaradiManoj <manojvaradi18@gmail.com>
2026-08-14 12:59:56 +0530

    Add files via upload

---
+++ Database Schema.sql
@@ -0,0 +1,8 @@
+/* ======================================================================
+   BANKING SYSTEM DATABASE - T-SQL SCHEMA (SQL Server)
+   ====================================================================== */
+ 
+CREATE DATABASE BankingSystem;
+GO
+USE BankingSystem;
+GO

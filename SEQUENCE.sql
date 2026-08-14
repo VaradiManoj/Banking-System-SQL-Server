04fbe83f961562af20ffa5e670c2280f306a43c0
VaradiManoj <manojvaradi18@gmail.com>
2026-08-14 13:25:57 +0530

    Add files via upload

---
+++ SEQUENCE.sql
@@ -0,0 +1,9 @@
+/*----------------------------------
+  TRANSACTION REFERENCE SEQUENCE
+  ----------------------------------*/
+
+CREATE SEQUENCE TransactionReferenceSeq
+    AS INT
+    START WITH 1
+    INCREMENT BY 1;
+GO
